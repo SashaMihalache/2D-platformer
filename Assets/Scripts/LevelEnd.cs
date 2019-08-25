40f243e461afb1a2dbdc3fb63079bc7078de6b0a
sasha mihalache <sasha.mihalache@gmail.com>
2019-08-25 20:25:39 +0300

    add level exit component and move to next level

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+  public string levelToLoad;
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Player")
+    {
+      SceneManager.LoadScene(levelToLoad);
+    }
+  }
+}

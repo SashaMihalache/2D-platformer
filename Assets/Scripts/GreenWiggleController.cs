4a58da263c8a22242601711da7156d1711ab68f0
sasha mihalache <sasha.mihalache@gmail.com>
2019-08-25 11:26:43 +0300

    add green wiggle enemy, enemy tag, stomp enemies

---
+++ Assets/Scripts/GreenWiggleController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenWiggleController : MonoBehaviour
+{
+  public Rigidbody2D myRigidBody;
+  public Transform leftPoint;
+  public Transform rightPoint;
+  public float moveSpeed;
+  public bool movingRight;
+
+  void Start()
+  {
+    myRigidBody = GetComponent<Rigidbody2D>();
+  }
+
+  void Update()
+  {
+    if (movingRight && transform.position.x > rightPoint.position.x)
+    {
+      movingRight = false;
+    }
+    if (!movingRight && transform.position.x < leftPoint.position.x)
+    {
+      movingRight = true;
+    }
+    if (movingRight)
+    {
+      myRigidBody.velocity = new Vector3(moveSpeed, myRigidBody.velocity.y, 0f);
+    }
+    else
+    {
+      myRigidBody.velocity = new Vector3(-moveSpeed, myRigidBody.velocity.y, 0f);
+    }
+  }
+}
---
+++ Assets/Scripts/StompEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StompEnemy : MonoBehaviour
+{
+  public GameObject deathSplosion;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Enemy")
+    {
+      Destroy(other.gameObject);
+      Instantiate(deathSplosion, other.transform.position, other.transform.rotation);
+    }
+  }
+}
